3c9f054a39ca04aa083ec14728d1083b5b123768
SebastianSandi <daniel.perezmorera@ucr.ac.cr>
2023-05-13 23:21:52 -0600

    Nuevas pruebas para nachOs

---
+++ NachOSx64/code/test/RW.c
@@ -0,0 +1,12 @@
+#include "syscall.h"
+
+int main() {
+  char buffer[100];
+  int i = 0;
+  Create("../test/prueba.txt");
+  int fd = Open("../test/prueba.txt");
+  Read(buffer, 100, 0);
+  Write(buffer, 100, 1);
+  Write(buffer, 100, fd);
+  Close(fd);
+}
