b1b97ec530fc8082456360d232037279a1543267
Daniel Perez <daniel.perezmorera@ucr.ac.cr>
2023-07-05 07:52:31 -0600

    Prueba de programas de usuario

--- NachOSx64/code/test/RW.c
+++ NachOSx64/code/test/RW.c
@@ -1,13 +1,13 @@
 #include "syscall.h"
 
-
 int main() {
   char buffer[100];
   int i = 0;
+  Write("Test\n", 6, 1);
   Create("../test/prueba.txt");
   int fd = Open("../test/prueba.txt");
   Read(buffer, 100, 0);
-  Write(buffer, 100, 1);
+  // Write(buffer, 100, 1);
   Write(buffer, 100, fd);
-  Close(fd);
+  // Close(fd);
 }
